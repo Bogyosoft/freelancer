9c2b3385d7ad98033d49f8d3f49caaa8e3fdc4ee
DanMark <41143582+KissDanielMark@users.noreply.github.com>
2021-09-27 22:26:48 +0200

    Add files via upload

---
+++ iOS/Freelancer/Freelancer/FreelancerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreelancerApp.swift
+//  Freelancer
+//
+//  Created by Kiss Dániel Márk on 2021. 09. 27..
+//
+
+import SwiftUI
+
+@main
+struct FreelancerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
