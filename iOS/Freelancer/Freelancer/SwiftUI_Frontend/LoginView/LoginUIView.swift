831bd13d946bc5700a57764685fe32bfe26ba75f
Daniel Kiss <daniel.mark.kiss@gmail.com>
2021-10-12 12:43:08 +0200

    #5 Started working on LoginView

---
+++ iOS/Freelancer/Freelancer/SwiftUI_Frontend/LoginView/LoginUIView.swift
@@ -0,0 +1,33 @@
+//
+//  LoginUIView.swift
+//  Freelancer
+//
+//  Created by Kiss Dániel Márk on 2021. 10. 12..
+//
+
+import SwiftUI
+
+struct LoginUIView: View {
+    
+    @State private var eMailField: String = "Test"
+    @State private var passwordField: String = "test"
+    
+    var body: some View {
+        VStack
+        {
+            Text("Freelancer")
+            TextField("E-Mail", text: $eMailField)
+            SecureField("Jelszó", text: $passwordField)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Login")
+            }
+        }
+        
+    }
+}
+
+struct LoginUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginUIView()
+    }
+}
