67092df5674fdf4c2123e83b8a9ff27628b5cff6
Daniel Kiss <daniel.mark.kiss@gmail.com>
2021-10-13 10:28:19 +0200

    #5 Registration View started

--- iOS/Freelancer/Freelancer/FreelancerApp.swift
+++ iOS/Freelancer/Freelancer/FreelancerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FreelancerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginUIView()
         }
     }
 }
---
+++ iOS/Freelancer/Freelancer/SwiftUI_Frontend/RegistrationView/RegistartionUIView.swift
@@ -0,0 +1,20 @@
+//
+//  RegistartionUIView.swift
+//  Freelancer
+//
+//  Created by Kiss Dániel Márk on 2021. 10. 13..
+//
+
+import SwiftUI
+
+struct RegistartionUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegistartionUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistartionUIView()
+    }
+}
