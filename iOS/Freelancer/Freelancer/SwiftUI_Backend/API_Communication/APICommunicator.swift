ccc8deb36e6a89238cfaf32755ec3bb59e5bf513
Daniel Kiss <daniel.mark.kiss@gmail.com>
2021-10-26 21:54:05 +0200

    #8 POST is woking

--- iOS/Freelancer/Freelancer/SwiftUI_Backend/API_Communication/APICommunicator.swift
+++ iOS/Freelancer/Freelancer/SwiftUI_Backend/API_Communication/APICommunicator.swift
@@ -13,7 +13,33 @@ class APICommunicator
 {
     func GetRequest()
     {
+        /*"List": [
+            /*[
+                "IdQuestion" : 5,
+                "IdProposition": 2,
+                "Time" : 32
+            ],*/
+            /*[
+                "IdQuestion" : 4,
+                "IdProposition": 3,
+                "Time" : 9
+            ]*/
+        ]*/
+        
+        let parameters: [String: Any] = [
+            "id" : 102,
+            "password" : "ios",
+            "role" : "king",
+            "score": 0,
+            "username": "iOS"]
+        
+        
         AF.request("http://freelancerbackend-env.eba-34kjxuhr.eu-central-1.elasticbeanstalk.com/api/v1/user/", method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).response{response in
+            
+            debugPrint(response)
+        }
+        print("POST")
+        AF.request("http://freelancerbackend-env.eba-34kjxuhr.eu-central-1.elasticbeanstalk.com/api/v1/user/", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).response{response in
             
             debugPrint(response)
         }
