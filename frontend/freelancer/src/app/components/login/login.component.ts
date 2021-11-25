81819f243ce3471585a2b372344c3ebaff126e66
Gergő Bálint <gergo.balint@dynazol.com>
2021-11-25 10:48:37 +0100

    AuthGuard

--- frontend/freelancer/src/app/components/login/login.component.ts
+++ frontend/freelancer/src/app/components/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    this.authService.login();
   }
-
 }
--- frontend/freelancer/src/app/modules/app-routing.module.ts
+++ frontend/freelancer/src/app/modules/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from '../components/home/home.component';
 import { LoginComponent } from '../components/login/login.component';
 import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
 import { ProfileComponent } from '../components/profile/profile.component';
+import { AuthGuardService } from '../services/auth-guard.service';
 
 const routes: Routes = [
   {
@@ -21,6 +22,7 @@ const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent,
+    canActivate: [AuthGuardService]
   },
   {
     path: '**',
