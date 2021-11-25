623f8ba710d2d8c1b21a40875cf7568ed1247ddb
Gergő Bálint <gergo.balint@dynazol.com>
2021-11-25 10:16:23 +0100

    Modules moved

--- frontend/freelancer/src/app/modules/app-routing.module.ts
+++ frontend/freelancer/src/app/modules/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from '../components/home/home.component';
+import { LoginComponent } from '../components/login/login.component';
+import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
+import { ProfileComponent } from '../components/profile/profile.component';
 
 const routes: Routes = [
   {
